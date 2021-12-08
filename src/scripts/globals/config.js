628e445f711095aa5e9f3d16080589f8782cf233
rizatsk <rzsakmir@gmail.com>
2021-12-09 00:21:20 +0700

    mengganti api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'f90e61d915d316aa4b4321c8ccf3d2a516c8e0b63caa3651e3af8dae84b24e84',
+  KEY: '1757493be524ed4fcf39274ec5ecb0b40b3f60de117cce8bcc3eaac715486b32',
   BASE_URL: 'https://api.binderbyte.com/v1/track',
   CACHE_NAME: 'CariPaket-V1',
 };
