0bbc4103a93a4f32e8873dfbe5e5e990bbb33f87
rizatsk <rzsakmir@gmail.com>
2021-12-21 22:20:05 +0700

    memperbaiki tampilan about us

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Dashboard from '../views/pages/dashboard';
+import AboutUs from '../views/pages/about-us';
 
 const routes = {
   '/': Dashboard,
   '/dashboard': Dashboard,
+  '/about-us': AboutUs,
 };
 
 export default routes;
